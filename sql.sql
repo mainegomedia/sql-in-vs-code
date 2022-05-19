0f3f4b8cb72e6d65ee06395ce86630b65f3d40b8
mainegomedia <mainegomedia@gmail.com>
2022-05-18 20:56:08 -0400

    first commit

---
+++ sql.sql
@@ -0,0 +1,19 @@
+-- CREATE TABLE USERS (username TEXT NOT NULL PRIMARY KEY, 
+-- password NOT NULL, email NOT NULL);
+
+
+INSERT
+-- INSERT INTO users (username, password, email)
+-- VALUES ("jermaine", "ganoosh", "joe@gmail.com");
+
+-- SELECT username FROM users;
+-- WHERE username LIKE j%;
+
+-- UPDATE users SET email="notjermaine@gmail.com"
+-- WHERE username = "jermaine";
+
+-- SELECT email FROM users
+-- WHERE username LIKE "jermaine";
+
+DELETE FROM users
+WHERE username = "jermaine"
